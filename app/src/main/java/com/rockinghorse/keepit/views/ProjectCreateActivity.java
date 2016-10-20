f4a2f5575cbb895aebec042e26a097033a13681c
Tércio Garcia <tercio.garcia@smarti9.com.br>
2016-10-20 14:13:18 -0200

    Adding missing file ProjectCreateActivity.java

---
+++ app/src/main/java/com/rockinghorse/keepit/views/ProjectCreateActivity.java
@@ -0,0 +1,35 @@
+package com.rockinghorse.keepit.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.rockinghorse.keepit.R;
+
+public class ProjectCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_create);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == android.R.id.home){
+            onBackPressed();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
