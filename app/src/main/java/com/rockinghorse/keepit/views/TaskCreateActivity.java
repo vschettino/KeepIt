e51a5041d5f00f1391ac6ab7de002460781bf17b
Tércio Garcia <tercio.garcia@smarti9.com.br>
2016-10-20 20:55:29 -0200

    Doing layout and behavior ajustments in Project Create and Task Create actvities

---
+++ app/src/main/java/com/rockinghorse/keepit/views/TaskCreateActivity.java
@@ -0,0 +1,31 @@
+package com.rockinghorse.keepit.views;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.rockinghorse.keepit.R;
+
+public class TaskCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_task);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
